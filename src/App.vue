255cc1e35362f3374d828e754cacd44d18e1b55b
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-11-13 19:21:00 -0300

    testando

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Login from './views/Login.vue'
 </script>
 
 <template>
   <RouterView />
-  <Login />
 </template>
 
 <style scoped>
-#app {
-  background-color: black !important;
-}
 </style>
+
 <script>
-export default {
-  components: { Login },
-  
-}
+
 </script>
