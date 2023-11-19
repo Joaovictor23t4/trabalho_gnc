aaa104ba0afd06f19af16f2c8009f120c4a8d8ec
LuizBlume <luiz.fernando.251107@gmail.com>
2023-11-19 12:35:01 -0300

    login finalizado

--- src/views/LoginVue.vue
+++ src/views/LoginVue.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="container-pai">
+  <div class="container-pai">
     <nav class="navbar navbar-expand-lg bg-dark">
       <div class="container-fluid">
         <button
@@ -20,36 +20,40 @@
     </nav>
     <nav class="navbar bg-body-tertiary">
       <div class="container-fluid nav-cad">
-        <h1>Cadastre-se</h1>
+        <h1>Bem vindo de volta!</h1>
       </div>
     </nav>
   </div>
-    <div class="container text-center">
-        <div class="row">
-        <div class="col-sm-8">
-            <h1>Bem vindo de volta!</h1>
-            <h2>Para continuar, faça login em sua conta.</h2>
-            <div class="form">
-            <form action="">
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email" placeholder="Email" />
-                <label for="senha">Senha</label>
-                <input type="password" name="senha" id="senha" placeholder="Senha" />
-                <button type="submit" class="btn btn-primary">Entrar</button>
-                <router-link to="/">Esqueceu a senha?</router-link>
-                <hr />
-                <router-link to="/cadastro">Não tem uma conta? Cadastre-se</router-link>
-            </form>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-sm-4">
+        <img src="../gnc-logo.png" alt="" id="logo" />
+        <h3>Não tem uma conta? Crie agora mesmo!</h3>
+        <button type="button" class="btn btn-outline-secondary">Criar conta!</button>
+      </div>
+      <div class="col-sm-8">
+        <h1>Bem vindo de volta!</h1>
+        <h2>Para continuar, faça login em sua conta.</h2>
+        <div class="form">
+          <form action="">
+            <div class="inputBox">
+              <input type="text" name="email" id="email" class="inputUser" required />
+              <label for="email" class="labelInput">Email</label>
             </div>
+            <div class="inputBox">
+            <input type="password" name="senha" id="senha" class="inputUser" required maxlength="30"/>
+            <label for="senha" class="labelInput">Senha</label>
+          </div>
+            <button type="button" class="btn btn-outline-danger">Entrar</button>
+            <router-link to="/">Esqueceu a senha?</router-link>
+            <hr />
+            <router-link to="/cadastro">Não tem uma conta? Cadastre-se</router-link>
+          </form>
         </div>
-        <div class="col-sm-4">
-            <img src="../gnc-logo.png" alt="" id="logo" />
-            <h3>Não tem uma conta? Crie agora mesmo!</h3>
-            <button type="button" class="btn btn-outline-secondary">Criar conta!</button>
-        </div>
-        </div>
+      </div>
     </div>
-    <footer>
+  </div>
+  <footer>
     <div class="container-footer">
       <div class="row">
         <div class="col-12">
@@ -83,43 +87,42 @@ body {
   align-items: flex-start !important;
   height: 100px !important;
 }
-h1 {
-    color: white !important;
-}
+
 .container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 44px;
 }
 .row {
   display: flex !important;
   flex-direction: row !important;
   justify-content: center !important;
-  width: 80vh !important;
+  width: 70% !important;
 }
 #logo {
   width: 80%;
   height: 25vh;
 }
 h1 {
-  color: black !important;
+  color: white !important;
 }
-#email,
-#senha {
-  color: #000000 !important;
-}
-input {
-  background-color: #f3ebeb !important;
-}
+
 .col-sm-4 {
   background-color: #000000 !important;
   border: #333333 0.5px solid !important;
+  border-bottom-left-radius: 10px !important;
+  border-top-left-radius: 10px !important;
+  padding: 10px;
 }
 .col-sm-8 {
   color: #a5a5a5 !important;
-  background-color: #fffafa !important;
+  background-color: #272727 !important;
   box-shadow: 1px 0px 4px 0px white !important;
+  border-bottom-right-radius: 10px !important;
+  border-top-right-radius: 10px !important;
+  padding: 20px;
 }
 .form {
   display: flex;
@@ -149,7 +152,58 @@ input {
   color: white;
 }
 
+h3 {
+  color: white !important;
+}
+button {
+  color: white !important;
+}
 .col-12 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+  width: 100%;
+  background-color: #212529;
+  color: white;
+}
+.inputBox {
+  position: relative;
+}
+.inputUser {
+  background: none;
+  border: none;
+  border-bottom: 1px solid white;
+  outline: none;
+  color: white !important;
+  font-size: 15px;
+  width: 100% !important;
+  letter-spacing: 2px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  white-space: pre-line;
+  margin: 30px 0 30px 0;
+}
+#email, #senha {
+  color: white !important;
+}
+
+.labelInput {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  pointer-events: none;
+  transition: 0.5s;
+}
+
+.inputUser:focus ~ .labelInput,
+.inputUser:valid ~ .labelInput {
+  top: -20px;
+  font-size: 12px;
+  color: #018d5c;
+}
+.container-footer {
   display: flex;
   justify-content: center;
   align-items: center;
