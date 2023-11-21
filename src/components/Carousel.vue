97e76abad4305a044e0d030336362e2909920f96
Joaovictor23t4 <joaovictor239090@gmail.com>
2023-11-21 15:16:02 -0300

    HotFix: concertando erros do carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,28 +1,41 @@
 <template>
-<div id="carouselExampleIndicators" class="carousel slide">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
-    <div v-for="(item, itemIndex) in destaquesJson" :key="itemIndex">
-        <div v-for="(filme, filmeIndex) in item.event" :key="filmeIndex">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img :src="filme.images[1].url" class="d-block w-100" alt="...">
-                </div>
-              </div>
-        </div>
+  <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button v-for="(botao, botaoIndex) in destaquesJson" :key="botaoIndex" type="button" data-bs-target="#carouselExampleIndicators" :data-bs-slide-to="botaoIndex" class="active" aria-current="true" :aria-label="'Slide ' + botaoIndex"
+      ></button>
     </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+
+    <div v-for="(item, itemIndex) in destaquesJson" :key="itemIndex">
+      <div v-for="(filme, filmeIndex) in item.event.images" :key="filmeIndex">
+        <div v-if="filme.type === 'PosterHorizontal'" class="carousel-inner">
+          <div v-if="itemIndex === 0" class="carousel-item active">
+            <img :src="filme.url" class="d-block w-100" alt="..." />
+          </div>
+          <div v-else class="carousel-item">
+            <img :src="filme.url" class="d-block w-100" alt="..." />
+          </div>
+        </div>
+      </div>
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 </template>
 
 <script setup>
