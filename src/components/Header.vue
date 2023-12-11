fe2efd17401863482b3e9d4937a59ff5abb36408
LuizBlume <luiz.fernando.251107@gmail.com>
2023-12-11 13:29:23 -0300

    remoção de dropdown

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,88 +3,100 @@
     <nav id="nav">
       <div id="nav-principal">
         <div id="bloco1">
-          <router-link to="/"><img src="/src/assets/images/gnc-logo-2.png" alt="" id="logo"></router-link>
-          <div id="container-shopping" v-show="pathAtual !== '/tela-filme'">;
+          <router-link to="/"
+            ><img src="/src/assets/images/gnc-logo-2.png" alt="" id="logo"
+          /></router-link>
+          <div id="container-shopping" v-show="pathAtual !== '/tela-filme'">
+            ;
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" id="icon-shopping" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
-                  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
-                  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                  <i class="bi bi-geo-alt"></i>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                id="icon-shopping"
+                fill="currentColor"
+                class="bi bi-geo-alt"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"
+                />
+                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                <i class="bi bi-geo-alt"></i>
               </svg>
             </span>
             <select id="selectShopping" v-model="selectedShopping" @change="handleSelectChange">
               <option value="" disabled>Escolha seu shopping</option>
               <option value="mueller" selected>Mueller</option>
               <option value="garten">Garten</option>
-          </select>
+            </select>
           </div>
         </div>
         <div id="bloco2">
-              <div id="container-search">
-                <input type="search" placeholder="Pesquisar filme" id="inSearch"/>
-                <span>
-                  <svg xmlns="http://www.w3.org/2000/svg" id="icon-search" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-                    <i class="bi bi-search"></i>
-                  </svg>
-                </span>
-             </div>
+          <div id="container-search">
+            <input type="search" placeholder="Pesquisar filme" id="inSearch" />
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                id="icon-search"
+                fill="currentColor"
+                class="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"
+                />
+                <i class="bi bi-search"></i>
+              </svg>
+            </span>
+          </div>
         </div>
         <div id="bloco3">
           <div id="container-usuario">
             <span>
-              <svg xmlns="http://www.w3.org/2000/svg" id="icon-user" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                id="icon-user"
+                fill="currentColor"
+                class="bi bi-person-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                />
                 <i class="bi bi-person-circle"></i>
               </svg>
             </span>
-            <button
-                class="btn btn-secondary dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Entre ou cadastre-se
-              </button>
-              <ul class="dropdown-menu">
-                <router-link to="/cadastro" id="btnUsuario">
-                  <li class="dropdown-item">Cadastre-se</li>
-                </router-link>
-                <router-link to="/login" id="btnUsuario">
-                  <li class="dropdown-item">Entrar</li>
-                </router-link>
-              </ul>
           </div>
-          </div>
+        </div>
       </div>
     </nav>
   </header>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { useMovieStore } from '../stores/movie'
 
-const selectedShopping = ref("mueller");
+const selectedShopping = ref('mueller')
 
-const router = useRouter();
-const pathAtual = router.currentRoute.value.path;
+const router = useRouter()
+const pathAtual = router.currentRoute.value.path
 
-const storeMovie = useMovieStore();
+const storeMovie = useMovieStore()
 
-storeMovie.cine = selectedShopping.value;
+storeMovie.cine = selectedShopping.value
 
-console.log(pathAtual);
+console.log(pathAtual)
 
 // Utilize o defineEmits diretamente para declarar os eventos que serão emitidos
-const emit = defineEmits(['parametroShopping']);
+const emit = defineEmits(['parametroShopping'])
 
 function handleSelectChange(event) {
-  selectedShopping.value = event.target.value;
-  storeMovie.cine = event.target.value;
-  emit('parametroShopping', selectedShopping.value);
+  selectedShopping.value = event.target.value
+  storeMovie.cine = event.target.value
+  emit('parametroShopping', selectedShopping.value)
   // console.log(selectedShopping.value)
 }
 </script>
@@ -99,7 +111,7 @@ function handleSelectChange(event) {
   display: flex;
   justify-content: space-around;
   align-items: center;
-  background-color: #161F2E;
+  background-color: #161f2e;
 }
 
 #bloco1 {
@@ -136,7 +148,7 @@ button:hover {
   border: transparent;
 }
 .dropdown-menu {
-  background-color: #161F2E;
+  background-color: #161f2e;
   border: none;
   border-radius: 0;
   box-shadow: none;
@@ -200,12 +212,12 @@ button:hover {
   height: 35px;
   border-radius: 15px;
   padding: 0 16px;
-  background-color: #2A364A;
-  color: #424F64;
+  background-color: #2a364a;
+  color: #424f64;
 }
 
 #inSearch::placeholder {
-  color: #424F64;
+  color: #424f64;
 }
 
 #logo {
@@ -329,7 +341,7 @@ button:hover {
   #bloco2 {
     width: 15%;
   }
-  
+
   #container-search > input {
     display: none;
   }
@@ -402,7 +414,7 @@ button:hover {
   #bloco2 {
     width: 15%;
   }
-  
+
   #container-search > input {
     display: none;
   }
